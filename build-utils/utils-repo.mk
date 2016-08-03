657799550f8db6792b7296f0ee0ef3f70c9270a3
Grigory Antsiferov <azr@bakka.su>
2016-08-03 17:12:51 +0300

    * build-utils/utils-repo.mk   Update to handle % in key paths.

--- build-utils/utils-repo.mk
+++ build-utils/utils-repo.mk
@@ -2,11 +2,10 @@ REPO_INIT := build-utils/repo_init.sh
 IMAGES_SHARED := $(shell echo "${HOME}")/.cache/rbkmoney/images/shared
 GITHUB_PRIVKEY ?=
 GITHUB_URI_PREFIX := git+ssh://github.com
-GITHUB_SSH_COMMAND := $(shell which ssh) -o User=git -o StrictHostKeyChecking=no $(if $(GITHUB_PRIVKEY),-i $(GITHUB_PRIVKEY),)
 
 BAKKA_SU_PRIVKEY ?=
+BAKKA_SU_PRIVKEY := $(shell echo $(GITHUB_PRIVKEY) | sed -e 's|%|%%|g')
 BAKKA_SU_URI_PREFIX := $(if $(BAKKA_SU_PRIVKEY),git+ssh,git)://git.bakka.su
-BAKKA_SU_SSH_COMMAND := $(shell which ssh) -o User=git -o StrictHostKeyChecking=no $(if $(BAKKA_SU_PRIVKEY),-i $(BAKKA_SU_PRIVKEY),)
 
 # portage
 $(IMAGES_SHARED)/portage/.git: .git

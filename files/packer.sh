2c78437c8f77d5a6fb25187f2d55fe5606897e16
 Timur Izhbulatov <t.izhbulatov@rbkmoney.com>
2017-10-20 18:02:21 +0300

    WIP

--- files/packer.sh
+++ files/packer.sh
@@ -4,7 +4,9 @@ source /lib/gentoo/functions.sh
 export EMERGE="emerge -q"
 SALT_VERSION="2015.8.13"
 
+# Get utf-8 support with default locale
 ebegin "Setting locales to generate"
+# DK uses 24h time
 cat <<EOF> /etc/locale.gen
 en_DK.UTF-8 UTF-8
 EOF
@@ -21,6 +23,7 @@ locale-gen || exit $?
 
 eselect locale set en_DK.utf8 || exit $?
 
+# Get cert for loading packages from bakka repo via HTTPS
 ebegin "Downloading CA for the package repository"
 mkdir -p /usr/local/share/ca-certificates \
     && wget -q http://bakka.su/ca/baka_bakka.crt -O /usr/local/share/ca-certificates/baka_bakka.crt
@@ -29,6 +32,7 @@ ebegin "Updating CA cerificates"
 update-ca-certificates --fresh > /dev/null
 eend $? "Failed" || exit $?
 
+# /tmp/data mount set in packer.json
 ebegin "Copying portage/make.conf"
 cp /tmp/data/portage.make.conf /etc/portage/make.conf
 eend $? "Failed" || exit $?
@@ -46,44 +50,46 @@ auto-sync = false
 EOF
 eend $? "Failed" || exit $?
 
+# https://wiki.gentoo.org/wiki/Profile_(Portage)
 ebegin "Selecting profile"
 eselect profile set hardened/linux/amd64/no-multilib
 eend $? "Failed" || exit $?
 
 ebegin "Setting bootstrap flags"
 mkdir -p /etc/portage/package.{accept_keywords,keywords,use,env} \
-    && cat <<EOF>> /etc/portage/package.keywords/bootstrap
-=app-admin/salt-${SALT_VERSION} ~amd64
-net-libs/zeromq:0/5 ~amd64
-<dev-python/pyzmq-16 ~amd64
-dev-python/cffi ~amd64
-EOF
+    && touch /etc/portage/package.keywords/bootstrap
 eend $? "Failed" || exit $?
 
+# XXX we may need to remove openssh
 ebegin "Removing openssh (to skip it's rebuilding with -bindist)"
 ${EMERGE} -C openssh
 eend $? "Failed" || exit $?
 
+# bakka.su cert is ECDSA and to get this supported we rebuild openssl without
+# bindist
 ebegin "Rebuilding openssl -bindist"
 FEATURES="-getbinpkg" ${EMERGE} --verbose=n openssl
 eend $? "Failed" || exit $?
 
+# Enable bakka.su mirrors
 ebegin "Uncommenting GENTOO_MIRRORS and other vars in make.conf"
 sed -i "s|\# sed-remove||g" /etc/portage/make.conf
 eend $? "Failed" || exit $?
 
-ebegin "Emerging localepurge salt qemacs nvi openssh"
-${EMERGE} --verbose=n ">=zeromq-4.1" "=app-admin/salt-${SALT_VERSION}" qemacs nvi app-admin/localepurge
+ebegin "Emerging localepurge qemacs nvi openssh"
+${EMERGE} --verbose=n qemacs nvi app-admin/localepurge
 eend $? "Failed" || exit $?
 
-ebegin "Selecting python2.7 as default python interpreter"
-eselect python set python2.7
-eend $? "Failed" || exit $?
+# XXX python2.7 is needed by salt
+#ebegin "Selecting python2.7 as default python interpreter"
+#eselect python set python2.7
+#eend $? "Failed" || exit $?
 
 ebegin "Selecting pager"
 eselect pager set /usr/bin/less
 eend $? "Failed" || exit $?
 
+# XXX perl is needed by at least localepurge
 einfo "Updating perl"
 perl-cleaner --reallyall || exit $?
 
@@ -91,18 +97,13 @@ ebegin "Updating world"
 ${EMERGE} -uDN @world
 eend $? "Failed" || exit $?
 
+# remove orphaned deps
 ebegin "Cleaning deps"
 ${EMERGE} --verbose=n --depclean
 eend $? "Failed" || exit $?
 
 einfo "Purging extra locales"
 localepurge || exit $?
-
-if [ ! -d /var/salt ]; then
-    ebegin "Creating /var/salt"
-    mkdir -p /var/salt
-    eend $? || exit $?
-fi
 
 find /usr/share/gtk-doc -delete
 find /usr/share/man -delete

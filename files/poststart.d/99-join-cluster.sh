b61e6fcc5ba7e7378548d0e0b8d4d8a464b27c67
Sergey Yelin <elinsn@gmail.com>
2020-02-07 19:39:23 +0300

    Join via REST is broken, use riak-admin

--- files/poststart.d/99-join-cluster.sh
+++ files/poststart.d/99-join-cluster.sh
@@ -5,8 +5,11 @@ if [[ -z "$($RIAK_ADMIN cluster status | egrep $COORDINATOR_NODE_HOST)" && "$COO
   # Not already in this cluster, so join
   echo "Connecting to cluster coordinator $COORDINATOR_NODE"
   # Token is any string, that can be used to bypass CSRF
-  TOKEN=$(/usr/bin/openssl rand -hex 12)
-  curl -sSL -X POST $HOST:8098/admin/cluster \
-       -d "{\"changes\":[{\"action\":\"join\",\"node\":\"$CLUSTER_NAME@$COORDINATOR_NODE_HOST\"}]}" \
-       -H "Content-Type: application/json" -b "csrf_token=$TOKEN" -H "X-CSRF-Token: $TOKEN"
+#  TOKEN=$(/usr/bin/openssl rand -hex 12)
+#  curl -sSL -X POST $HOST:8098/admin/cluster \
+#       -d "{\"changes\":[{\"action\":\"join\",\"node\":\"$CLUSTER_NAME@$COORDINATOR_NODE_HOST\"}]}" \
+#       -H "Content-Type: application/json" -b "csrf_token=$TOKEN" -H "X-CSRF-Token: $TOKEN"
+  riak-admin cluster join $CLUSTER_NAME@$COORDINATOR_NODE_HOST
+  riak-admin cluster plan
+  riak-admin cluster commit
 fi
